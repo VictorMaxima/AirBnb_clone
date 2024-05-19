efa472790d6f6fe3ea692e19d0b7f736f6795462
VictorMaxima <victormaxchidera@gmail.com>
2024-05-19 20:12:32 +0100

    try2

--- console.py
+++ console.py
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/python
 
 """ 
 the console for the airbnb project
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """ contains unittests for the File Storage class """
 
 import os
